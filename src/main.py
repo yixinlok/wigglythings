rotation on instance fix

src/main.py
@@ -68,8 +68,8 @@ def callback():
         time_step += 1
 
         ''' update the base first'''
-        if time_step > 10:
-            t = 10*time_step_size
+        if time_step > 20:
+            t = 20*time_step_size
         else: 
             t = time_step*time_step_size
             
@@ -86,13 +86,19 @@ def callback():
 
             bm_update_v(base_mesh, spin_base)
         elif globals.MOVE == "slam":
+            R_y = np.array([
+                [1.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0],
+                [0.0, 0.0, 1.0]
+            ], dtype=np.float32)
+
             displace_base = base_mesh.resting_v + np.array([0,0,0.5*np.sin(5*t)])
             bm_update_v(base_mesh, displace_base)
         
         ps_base_mesh = ps.register_surface_mesh("base mesh", base_mesh.v_cur, base_mesh.all_f)
 
         '''then update the instances'''
-        wp_update_all_instances(base_mesh,base_instance,instances_object)
+        wp_update_all_instances(base_mesh,base_instance,instances_object, R_y.T)
         # wp_update_all_instances(base_mesh,tear_base_instance,tear_instances_object)
 
         tets = base_instance.tets
@@ -144,8 +150,8 @@ else:
 
         for time_step in range(NUM_FRAMES):
             ''' update the base first'''
-            if time_step > 10:
-                t = 10*time_step_size
+            if time_step > 20:
+                t = 20*time_step_size
             else: 
                 t = time_step*time_step_size
                 
@@ -165,7 +171,7 @@ else:
                 displace_base = base_mesh.resting_v + np.array([0,0,0.5*np.sin(5*t)])
                 bm_update_v(base_mesh, displace_base)
             
-            wp_update_all_instances(base_mesh,base_instance,instances_object)
+            wp_update_all_instances(base_mesh,base_instance,instances_object, R_y.T)
             
         
             if POLYSCOPE_OR_USD == "usd":

src/step.py
@@ -11,7 +11,8 @@ import torch
 def wp_update_all_instances(
         bm: BaseMesh,
         bi: InstanceBase,
-        ix: Instances
+        ix: Instances,
+        R_y
     ):
 
     EV_LENGTH = wp.constant(bi.boundary_v.shape[0]*3)
@@ -98,10 +99,12 @@ def wp_update_all_instances(
               outputs=[face_points], 
               device=DEVICE)
     
-    # change it to wp torch in base instance
+    # reason im going from numpy to torch is because it converts it into a 3d array instead of a matric
     base_v = wp.from_torch(torch.from_numpy(bi.boundary_v).to(dtype=torch.float32).to(DEVICE))
     vs = wp.zeros((ix.num_instances, bi.boundary_v.shape[0],3), dtype=wp.float32, device=DEVICE)
     BI_NUM_V = wp.constant(bi.boundary_v.shape[0])
+
+    base_rotate = wp.from_torch(torch.from_numpy(R_y.astype(np.float32)).to(DEVICE))
 
     @wp.kernel
     def wp_compute_new_spikes(
@@ -109,24 +112,33 @@ def wp_update_all_instances(
         base_v: wp.array2d(dtype=float), # bi.v.shape[0],3
         rot_matrices_T: wp.array3d(dtype=float),# ix.num_instances,3,3
         face_points: wp.array2d(dtype=float), # ix.num_instances,3
-        vs: wp.array3d(dtype=float) # ix.num_instances,bi.v.shape[0],3
+        base_rotate: wp.array2d(dtype=float), # 3,3
+        vs: wp.array3d(dtype=float), # ix.num_instances,bi.v.shape[0],3
         ):
         i = wp.tid()
-
+        
         # displace = wp.tile_load(displaces[i], shape=(1, 3), offset=(j,0))
         # base = wp.tile_load(base_v, shape=(1, 3), offset=(j,0))
         displace = wp.tile_load(displaces[i], shape=(BI_NUM_V, 3), offset=(0,0))
         base = wp.tile_load(base_v, shape=(BI_NUM_V, 3), offset=(0,0))
         displaced_base_v = base + displace
+        
+        # base_rotate 
+        new_v_1 = wp.tile_zeros(shape=(BI_NUM_V, 3), dtype=float)
+        rot_mat = wp.tile_load(base_rotate, shape=(3, 3), offset=(0, 0))
+        wp.tile_matmul(displaced_base_v, rot_mat, new_v_1)
 
+        # face rotate
         # new_v = wp.tile_zeros(shape=(1, 3), dtype=float)
         new_v = wp.tile_zeros(shape=(BI_NUM_V, 3), dtype=float)
         rot_mat = wp.tile_load(rot_matrices_T[i], shape=(3, 3), offset=(0, 0))
-        wp.tile_matmul(displaced_base_v, rot_mat, new_v)
+        wp.tile_matmul(new_v_1, rot_mat, new_v)
 
+ 
         face_point = wp.tile_load(face_points, shape=(1,3), offset=(i,0))
         face_point = wp.tile_broadcast(face_point, shape=(BI_NUM_V, 3))
         new_v = new_v + face_point
+
         wp.tile_store(vs[i], new_v)
     wp.launch_tiled(wp_compute_new_spikes, 
                     # dim=(ix.num_instances, bi.v.shape[0]),
@@ -134,7 +146,8 @@ def wp_update_all_instances(
                     inputs=[displaces, 
                             base_v, 
                             rot_matrices_T_array3d, 
-                            face_points],
+                            face_points,
+                            base_rotate],
                     outputs=[vs],
                     block_dim=128,
                     device=DEVICE)